e1e6da2711df98a6362c9b24e0f1972cd1d2a758
Sophie Liu <sliu50@stuy.edu>
2025-01-06 23:58:27 -0500

    added basic server/client

--- basic_client.c
+++ basic_client.c
@@ -1,9 +1,16 @@
 #include "pipe_networking.h"
 
 int main() {
-
   int to_server;
-  int from_server;
+  int from_server = client_handshake(&to_server);
 
-  from_server = client_handshake( &to_server );
+  while(1){
+    int server_number;
+    if (read(from_server, &server_number, sizeof(server_number)) == 0) {
+      close(to_server);
+      close(from_server);
+      return(1);
+    }
+    printf("%d\n", server_number);
+  }
 }
--- basic_server.c
+++ basic_server.c
@@ -1,8 +1,14 @@
 #include "pipe_networking.h"
+#include <time.h>
 
 int main() {
   int to_client;
-  int from_client;
+  int from_client = server_handshake(&to_client);
 
-  from_client = server_handshake( &to_client );
+  while(1) {
+    srand(time(NULL));
+    int random_int = rand();
+    write(to_client, &random_int, sizeof(random_int));
+    sleep(1);
+  }
 }
