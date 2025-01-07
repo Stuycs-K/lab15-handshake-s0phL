495fef3fbacc58059dd12df287689c580fb227c5
Sophie Liu <sliu50@stuy.edu>
2025-01-07 00:36:15 -0500

    reordered a line of code

--- basic_server.c
+++ basic_server.c
@@ -2,11 +2,12 @@
 #include <time.h>
 
 int main() {
+  srand(time(NULL));
+
   int to_client;
   int from_client = server_handshake(&to_client);
 
   while(1) {
-    srand(time(NULL));
     int random_int = rand();
     write(to_client, &random_int, sizeof(random_int));
     sleep(1);
